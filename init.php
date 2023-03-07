4a7cf635d274637329a713c233e1fed29da9a460
Tais Guissi <71243004+TaisGuissi@users.noreply.github.com>
2023-03-06 22:29:53 -0300

    Controller

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+
+//timezone
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// conexão com o banco de dados
+
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','root');
+define('BD_SENHA','11171100952');
+define('BD_BANCO','livraria');
+
+?>
